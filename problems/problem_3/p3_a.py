dc0f93fbb32daae167ab0fce14d034f0a2199d64
Matthew Shen <mds377@cornell.edu>
2023-09-21 12:18:48 -0400

    base files from hw assignment

---
+++ problems/problem_3/p3_a.py
@@ -0,0 +1,13 @@
+'''
+Problem 3a
+
+input: 
+    file -- contains 2 lines. The first one has an integer n.
+    The second one has an ordering of the integers from 1 to n (see README). 
+    delta -- bound for ranking significance.
+output: Number of large inversions. 
+
+TODO: implement a Θ(n^2) as described in the homework.
+'''
+def number_of_large_inversions_3a(file, delta) -> int:
+    pass
---
+++ problems/problem_3/p3_b.py
@@ -0,0 +1,13 @@
+'''
+Problem 3b
+
+input: 
+    file -- contains 2 lines. The first one has an integer n.
+    The second one has an ordering of the integers from 1 to n (see README). 
+    delta -- bound for ranking significance.
+output: Number of large inversions. 
+
+TODO: implement a Θ(n log n) as described in the homework.
+'''
+def number_of_large_inversions_3b(file, delta) -> int:
+    pass
