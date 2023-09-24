dbe2b901e73f9f5f85cf6e0209d9e345cbd4ccfd
Matthew Shen <mds377@cornell.edu>
2023-09-24 13:41:22 -0400

    problem2 and most of problem 1

--- problems/problem_1/p1_c.py
+++ problems/problem_1/p1_c.py
@@ -23,27 +23,8 @@ def stable_matching_1c(file) -> dict:
     
     # doctors to hospitals map
     pairs = {} 
-    matched_hospitals = set()
+    
+    
+        
 
-    while len(matched_hospitals) < n:
-        for h in range(n):
-            if h not in matched_hospitals:
-                doc_preference = hospitals_pref[h].pop(0)
-
-                # If the hospital's preference hasn't been matched
-                if doc_preference not in pairs:
-                    pairs[doc_preference] = h
-                    matched_hospitals.add(h)
-
-                # If the hospital's preference already has a match
-                else:
-                    h2 = pairs[doc_preference]
-                    h1_idx = doctors_pref[doc_preference].index(h)
-                    h2_idx = doctors_pref[doc_preference].index(h2)
-                    if h1_idx < h2_idx:
-                        pairs[doc_preference] = h
-                        matched_hospitals.add(h)
-                        matched_hospitals.remove(h2)
     return pairs
-
-print(stable_matching_1c("input/test_p1_public_n2.txt"))
--- problems/problem_2/p2.py
+++ problems/problem_2/p2.py
@@ -7,4 +7,23 @@ output: List of list of integers composing the covering.
 TODO: implement a correct greedy algorithm from the homework.
 '''
 def interval_covering(M: int, intervals: list) -> list:
-    pass
+    c = max(intervals)[1]
+    J_lst = []
+
+    while len(intervals)>0:
+        
+        for interval in intervals:
+            if max(interval) < c:
+                pass
+            else:
+                intervals.remove(interval)
+
+        if len(intervals) > 0:
+            latest_finish_lst = max(intervals)
+            J_lst.append(latest_finish_lst)
+            intervals.remove(latest_finish_lst)
+
+            c = latest_finish_lst[0]
+            
+            
+    return J_lst
