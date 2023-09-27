8ea255b5e3720ba0089ae2fc1f1b7aa45ab76dec
NamanChimnani <nc1998@nyu.edu>
2023-09-26 21:52:03 -0400

    challenge question part a

--- problems/challenge/challenge.py
+++ problems/challenge/challenge.py
@@ -1,9 +1,20 @@
 import math
 
 
-def reference_multiply(x, y):
-    pass
+def reference_multiply(x: list, y:list):
+    def convert_to_int(lst):
+        result = 0
+        for i in range(len(lst)):
+            result += lst[i] * (2 ** i)
+        return result
+    result = convert_to_int(x) * convert_to_int(y)
 
+    res_list = []
+    while result > 0:
+        res_list.append(result % 2)
+        result = result // 2
+    res_list.reverse()
+    return res_list
 
 def karatsuba(x, y):
     pass
