f7f72273b81cfd2390aa7f47a50718740ab5b96c
NamanChimnani <nc1998@nyu.edu>
2023-09-26 20:15:24 -0400

    It works now

--- problems/problem_3/p3_b.py
+++ problems/problem_3/p3_b.py
@@ -10,50 +10,96 @@ output: Number of large inversions.
 TODO: implement a Θ(n log n) as described in the homework.
 '''
 def number_of_large_inversions_3b(file, delta) -> int:
+    def helper(lst, delta):
+        if len(lst) == 1:
+            return lst, 0
+        else:
+            a_lst = lst[:len(lst)//2]
+            b_lst = lst[len(lst)//2:]
+
+            # Sort and count
+            a_lst, inv_left = helper(a_lst, delta)
+            b_lst, inv_right = helper(b_lst, delta)
+
+            inv = inv_left + inv_right
+            # merge and count
+            i = 0
+            j = 0
+            while i < len(a_lst) and j < len(b_lst):
+                if a_lst[i] > b_lst[j] + delta:
+                    inv += len(a_lst) - i
+                    j += 1
+                else:
+                    i += 1
+
+            merge_lst = a_lst + b_lst
+            merge_lst.sort()
+
+            return merge_lst, inv
+
     n = 0
-    
     with open(file, "r") as f:
         n = int(f.readline())
         lst = [int(element) for element in list(f.readline().split())]
-    
-    if len(lst) == 1:
-        return 0
-    else:
-        inversions = 0
-        
-    # inversions = number_of_large_inversions_3a(lst,delta)
-        a_lst = lst[:len(lst)//2]
-        b_lst = lst[len(lst)//2:]
-        
-        # Sort and count
-        inversions = inversions + number_of_large_inversions_3a(a_lst,delta)
-#         # print(a_lst)
-        # print('a inv: ' + str(inversions))
-        inversions = inversions + number_of_large_inversions_3a(b_lst,delta)
-#         # print(b_lst)
-        # print('b inv: ' + str(inversions))
-        a_lst.sort()
-        b_lst.sort()
-        
-        
-        # merge and count
-        i = 0
-        j = 0
-        while i < len(a_lst) and j < len(b_lst):
-            
-            
-            if a_lst[i] > b_lst[j] + delta:
-                inversions += 1
-                j += 1
-            else:
-                i += 1
-        
-        # while j < len(b_lst):
-        #     if i > len(a_lst):
-        #         inversions += (len(b_lst)-j)
-        #         break
-            
-    return inversions   
+        ranks = list(range(n))
+        reorder_ranks = sorted(ranks, key=lambda x: lst[x])
+
+        _, inversions = helper(reorder_ranks, delta)
+    return inversions
+
+def number_of_large_inversions_3c(file, delta) -> int:
+    ordering = []
+    with open(file, "r") as f:
+        n = int(f.readline())
+        ranks = f.readline().split()
+        for x in ranks:
+            ordering.append(int(x))
+
+    ranks = list(range(n))
+    reorder_ranks = sorted(ranks, key=lambda x: ordering[x])
+
+    _, inversions = divide_and_conquer(reorder_ranks, delta)
+    return inversions
+
+# Θ(n log n) implementation, divide and conquer
+def divide_and_conquer(sub_ranks, delta) -> (list, int):
+    if len(sub_ranks) <= 1:
+        return sub_ranks, 0
+
+    mid = len(sub_ranks) // 2
+    left_ranks = sub_ranks[:mid]
+    right_ranks = sub_ranks[mid:]
+
+    left_sort, left_inv = divide_and_conquer(left_ranks, delta)
+    right_sort, right_inv = divide_and_conquer(right_ranks, delta)
+
+    i = j = inv = 0
+    while i < len(left_sort) and j < len(right_sort):
+        if left_sort[i] <= right_sort[j] + delta:
+            i += 1
+        else:
+            inv += len(left_sort) - i
+            j += 1
+
+    sorted_ordering = []
+    i = j = 0
+    while i < len(left_sort) and j < len(right_sort):
+        if left_sort[i] < right_sort[j]:
+            sorted_ordering.append(left_sort[i])
+            i += 1
+        else:
+            sorted_ordering.append(right_sort[j])
+            j += 1
+
+    while i < len(left_sort):
+        sorted_ordering.append(left_sort[i])
+        i += 1
+
+    while j < len(right_sort):
+        sorted_ordering.append(right_sort[j])
+        j += 1
+
+    return sorted_ordering, left_inv + right_inv + inv
 
 def number_of_large_inversions_3a(lst, delta) -> int:
     n = len(lst)
@@ -61,7 +107,11 @@ def number_of_large_inversions_3a(lst, delta) -> int:
     inversions = 0
         
     lst_sorted = sorted(lst)
-    
+    # q: explain what is happening here
+    # a: for each element in the sorted list, find the index of that element in the original list.
+    #    compare that index to the index of every element in the sorted list that comes after it.
+    #    if the index of the element in the sorted list is greater than the index of the element in the original list,
+    #    then we have an inversion.
     for x_val in lst_sorted:
         x_idx = lst_sorted.index(x_val)
         for y_val in lst_sorted[x_idx+1:]:
@@ -74,6 +124,6 @@ def number_of_large_inversions_3a(lst, delta) -> int:
 
 import os
 
-print(number_of_large_inversions_3b(os.path.dirname(os.getcwd()) +  r'/tests/input/test_p3_public_n4_1.txt',1))
+print(number_of_large_inversions_3b(os.path.dirname(os.getcwd()) +  r'/tests/input/test_p3_public_n4_2.txt',1))
 
 # ans 2
